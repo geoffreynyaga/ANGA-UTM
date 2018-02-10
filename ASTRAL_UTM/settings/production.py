3883ee1aea8238b0ea910c621f088bbdc7e701d3
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-10 11:03:27 +0300

    added Static root stuff to settings

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'ASTRAL_UTM.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -138,7 +138,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 CORS_REPLACE_HTTPS_REFERER      = True
