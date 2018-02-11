fe3a6faae2253f520b0ec5aff570b4ee57fde17a
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-11 23:00:59 +0300

    added whitenoise for static files

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -52,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
 ROOT_URLCONF = 'ASTRAL_UTM.urls'
@@ -143,6 +145,8 @@ STATICFILES_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 CORS_REPLACE_HTTPS_REFERER      = True
