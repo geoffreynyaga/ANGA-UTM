7aab20690272c8a86db96e518a9565a29c9cf137
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-12 19:36:39 +0300

    all

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -164,8 +164,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 LOGIN_URL = "accounts:login"
