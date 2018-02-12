9935b61f307f165367e729a62945bec9270a035e
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-12 22:50:08 +0300

    all

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -159,6 +159,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
