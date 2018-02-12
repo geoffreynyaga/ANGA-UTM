21d94eec9b0ff1ca2f50f4d3e70356a355b1975d
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-12 21:38:06 +0300

    all

--- ASTRAL_UTM/aws/conf.py
+++ ASTRAL_UTM/aws/conf.py
@@ -1,25 +1,27 @@
-# import datetime
-# AWS_ACCESS_KEY_ID = "<your_access_key_id>"
-# AWS_SECRET_ACCESS_KEY = "<your_secret_access_key>"
-# AWS_FILE_EXPIRE = 200
-# AWS_PRELOAD_METADATA = True
-# AWS_QUERYSTRING_AUTH = True
+import datetime
+AWS_ACCESS_KEY_ID = "AKIAITF6NYN7N4ZKKWJA"
+AWS_SECRET_ACCESS_KEY = "cmzh0kC8BitIHbgNWiGf5K+8NSeYTo1tgDg5lwq7"
+AWS_FILE_EXPIRE = 200
+AWS_PRELOAD_METADATA = True
+AWS_QUERYSTRING_AUTH = True
 
-# DEFAULT_FILE_STORAGE = '<your-project>.aws.utils.MediaRootS3BotoStorage'
-# STATICFILES_STORAGE = '<your-project>.aws.utils.StaticRootS3BotoStorage'
-# AWS_STORAGE_BUCKET_NAME = '<your_bucket_name>'
-# S3DIRECT_REGION = 'us-west-2'
-# S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-# MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
-# MEDIA_ROOT = MEDIA_URL
-# STATIC_URL = S3_URL + 'static/'
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+DEFAULT_FILE_STORAGE = 'ASTRAL_UTM.aws.utils.MediaRootS3BotoStorage'
+STATICFILES_STORAGE = 'ASTRAL_UTM.aws.utils.StaticRootS3BotoStorage'
+AWS_STORAGE_BUCKET_NAME = 'astral-utm-bucket'
+S3DIRECT_REGION = 'us-west-2'
+S3_URL = '//%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_URL = '//%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+MEDIA_ROOT = MEDIA_URL
+STATIC_URL = S3_URL + 'static/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
-# two_months = datetime.timedelta(days=61)
-# date_two_months_later = datetime.date.today() + two_months
-# expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
+two_months = datetime.timedelta(days=61)
+date_two_months_later = datetime.date.today() + two_months
+expires = date_two_months_later.strftime("%A, %d %B %Y 20:00:00 GMT")
 
-# AWS_HEADERS = { 
-#     'Expires': expires,
-#     'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
-# }
+AWS_HEADERS = { 
+    'Expires': expires,
+    'Cache-Control': 'max-age=%d' % (int(two_months.total_seconds()), ),
+}
+
+from ASTRAL_UTM.aws.conf import *
