5a22c56a9fc901cf9fea9023f0b2ba07df8bb6a2
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-12 23:31:31 +0300

    all

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -218,3 +218,11 @@ SECURE_FRAME_DENY               = True
 
 
 from ASTRAL_UTM.aws.conf import *
+
+if not DEBUG:
+    AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+    STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+    STATIC_URL = S3_URL
