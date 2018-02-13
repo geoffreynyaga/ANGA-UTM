601644fdb368a9272aea6e24582a66981d92abff
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-13 23:49:48 +0300

    all

--- ASTRAL_UTM/urls.py
+++ ASTRAL_UTM/urls.py
@@ -50,9 +50,9 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += [
 
-        url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT,}),
+        url(r'^static/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.STATIC_ROOT,}),
 
-        url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,}),
+        url(r'^media/(?P<path>.*)$', django.views.static.serve, {'document_root': settings.MEDIA_ROOT,}),
 
     ]
 
