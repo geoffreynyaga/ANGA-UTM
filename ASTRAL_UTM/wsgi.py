008a61f28cd888bcb6d5460093265986218e6563
geoffreynyaga <geoffreynyagak@gmail.com>
2018-02-13 14:23:21 +0300

    all

--- ASTRAL_UTM/wsgi.py
+++ ASTRAL_UTM/wsgi.py
@@ -7,21 +7,21 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-# import os
-
-# from django.core.wsgi import get_wsgi_application
-
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ASTRAL_UTM.settings")
-
-# application = get_wsgi_application()
-
-
-
 import os
+
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ASTRAL_UTM.settings")
 
-from django.core.wsgi import get_wsgi_application
-from whitenoise.django import DjangoWhiteNoise
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+
+
+
+# import os
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ASTRAL_UTM.settings")
+
+# from django.core.wsgi import get_wsgi_application
+# from whitenoise.django import DjangoWhiteNoise
+# application = get_wsgi_application()
+# application = DjangoWhiteNoise(application)
