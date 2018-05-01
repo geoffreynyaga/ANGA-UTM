d3b49ae9cce769ec3ea024da9fd7f5f86ceeada4
geoffreynyaga <geoffreynyagak@gmail.com>
2018-05-01 12:07:44 +0300

    post windows 10 pro install

--- organizations/views.py
+++ organizations/views.py
@@ -23,13 +23,31 @@ class AllCompanyFlightLogs(ListView):
         user_organizations  = Organization.objects.filter(users=self.request.user)
 
         all_users = {}
+        users_list = []
         for org in user_organizations:
             x = org.users.all()
+            users_list.append(x)
             all_users.update({org:x})
 
-        context['all_company_logs']       = FlightLog.objects.filter(user=self.request.user)
+        refined_user_list = []
+        for user_queryset_per_org in users_list:
+            for user in user_queryset_per_org:
+                if not user in refined_user_list:
+                    refined_user_list.append(user)
+
+        all_company_logs = []
+
+        for org_user in refined_user_list:
+            x = (FlightLog.objects.filter(user=org_user))
+            if x:
+                for log in x:
+                    all_company_logs.append(log)
+
         context['my_affiliate_companies'] = Organization.objects.filter(users=self.request.user)
         context['my_postholds']           = PostHolder.objects.filter(user=self.request.user)
-        context['organization_and_users']           = all_users
+        context['organization_and_users'] = all_users
+        context['all_company_logs']       = all_company_logs
+        context['all_employees']          = refined_user_list
+
 
         return context
