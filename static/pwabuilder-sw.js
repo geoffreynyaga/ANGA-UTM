6688bf9e399b4ca038791399a4014fc169429fcc
geoffreynyaga <geoffreynyagak@gmail.com>
2018-06-13 15:23:18 +0300

    added webpush service worker registration to base.html similar to PWAserviceworker

--- static/pwabuilder-sw.js
+++ static/pwabuilder-sw.js
@@ -12,6 +12,28 @@ self.addEventListener('install', function(event) {
   }));
 });
 
+//WEBPUSH
+// Register event listener for the 'push' event.
+self.addEventListener('push', function (event) {
+  // Retrieve the textual payload from event.data (a PushMessageData object).
+  // Other formats are supported (ArrayBuffer, Blob, JSON), check out the documentation
+  // on https://developer.mozilla.org/en-US/docs/Web/API/PushMessageData.
+  var payload = event.data ? event.data.text() : { "head": "No Content", "Body": "No Content" },
+    data = JSON.parse(payload),
+    head = data.head,
+    body = data.body;
+
+  // Keep the service worker alive until the notification is created.
+  event.waitUntil(
+    // Show a notification with title 'ServiceWorker Cookbook' and use the payload
+    // as the body.
+    self.registration.showNotification(head, {
+      body: body
+    })
+  );
+});
+
+
 //If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener('fetch', function(event) {
   var updateCache = function(request){
