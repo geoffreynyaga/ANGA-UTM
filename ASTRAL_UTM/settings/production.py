9beffa0b4fd25ad60c23b452b754c25f87365a25
geoffreynyaga <geoffreynyagak@gmail.com>
2020-01-06 11:00:27 +0300

    allowed hosts

--- ASTRAL_UTM/settings/production.py
+++ ASTRAL_UTM/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["astral-utm.herokuapp.com"]
+ALLOWED_HOSTS = ["anga-utm.herokuapp.com"]
 
 
 # Application definition
