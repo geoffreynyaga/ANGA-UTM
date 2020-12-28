7599d1fc417ec5946f126ae4716118246ea32833
geoffreynyaga <geoffreynyagak@gmail.com>
2020-12-28 22:12:55 +0300

    minor edits

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -1,33 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding:utf-8 -*-
-##################################################################################
-# File: /mnt/c/Projects/ANGA UTM/src/accounts/api/views.py                       #
-# Project: MFUKO                                                                 #
-# Author: Geoffrey Nyaga Kinyua  at geoffrey@mfuko.co.ke                         #
-# -----                                                                          #
-# Last Modified: Monday December 28th 2020 9:54:58 pm                            #
-# Modified By: Geoffrey Nyaga Kinyua at geoffrey@mfuko.co.ke                     #
-# -----                                                                          #
-# This file should not be copied and/or distributed without the express          #
-# permission of MFUKO PAYMENTS SERVICES Ltd.                                     #
-#                                                                                #
-# Copyright (c) 2020 MFUKO PAYMENTS SERVICES Ltd.                                #
-# -----                                                                          #
-# HISTORY:                                                                       #
-##################################################################################
-# class ViewProfile(LoginRequiredMixin, generic.DetailView):
-#     template_name = "accounts/profile.html"
-#     model = UserProfile
-
-#     def get_context_data(self, *args, **kwargs):
-#         context = super(ViewProfile, self).get_context_data(**kwargs)
-#         pk = self.kwargs["pk"]
-#         thisuser = User.objects.get(pk=pk)
-#         org = thisuser.userprofile.organization
-#         context["myrpas"] = Rpas.objects.filter(organization=org)
-#         context["myflightlogs"] = FlightLog.objects.filter(user=thisuser)
-#         return context
-
 from rpas.models import Rpas
 from django.contrib.auth.models import User
 from rest_framework import generics
@@ -38,11 +8,7 @@ from accounts.api.serializers import (
     UserProfileUASListSerializer,
 )
 from accounts.models import UserProfile
-from flight_plans.api.serializers import (
-    FlightLogListSerializer,
-    FlightLogReserveAirspaceSerializer,
-)
-from flight_plans.models import FlightLog
+
 from applications.models import ReserveAirspace
 
 from django.contrib.auth import get_user_model
@@ -58,7 +24,6 @@ class UserProfileDetailAPIView(generics.RetrieveAPIView):
 
 class UserProfileFlightLogsListAPIView(generics.ListAPIView):
     serializer_class = UserProfileFlightLogSerializer
-    # lookup_field = "pk"
 
     def get_queryset(self):
         """
@@ -87,17 +52,13 @@ class UserProfileUASListAPIView(generics.ListAPIView):
 
     def get_queryset(self):
         """
-        This view should return a list of all the logs
+        This view should return a list of all the uas
         for the currently authenticated user.
         """
 
         try:
             url_pk = self.kwargs.get("pk")
             user_from_url = User.objects.get(pk=url_pk)
-
-            # thisuser = User.objects.get(pk=pk)
-            # org = thisuser.userprofile.organization
-            # Rpas.objects.filter(organization=org)
 
             if user_from_url == self.request.user:
 
